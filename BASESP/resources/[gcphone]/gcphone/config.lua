c54b71aef138027b7735fb7334787b81eb4b6652
Ítalo Iran <italow6@gmail.com>
2021-11-09 10:48:39 -0300

    gcphone

---
+++ BASESP/resources/[gcphone]/gcphone/config.lua
@@ -0,0 +1,7 @@
+emergencyNumbers = {
+  [911] = 'Policia',
+  [112] = "Samu",
+  ["taxi"] = "Táxi",
+  ["mechanic"] = "Mecânico",
+  ["ADM"] = "Admin"
+}
---
+++ BASESP/resources/[gcphone]/mysql-async/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+game 'gta5'
+
+server_scripts {
+	'MySQLAsync.net.dll',
+	'lib/init.lua'
+}
+
+
