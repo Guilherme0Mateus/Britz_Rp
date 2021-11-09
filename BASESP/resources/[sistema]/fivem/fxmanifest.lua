3e00cd7fc280ce6d449eaa3f06a37e51a7866824
Ítalo Iran <italow6@gmail.com>
2021-11-09 15:06:23 -0300

    voice

---
+++ BASESP/resources/[sistema]/fivem/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'bodacious'
+game 'gta5'
+
+resource_type 'gametype' { name = 'Roleplay' }
+
+client_script 'fivem_client.lua'
+
+
---
+++ BASESP/resources/[sistema]/los-anjos/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'bodacious'
+game 'gta5'
+
+resource_type 'map' { gameTypes = { fivem = true } }
+
+map 'map.lua'
+
+
